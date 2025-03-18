e533070bd496393e43bfa3bbfa3d5b2323fb0bb5
ninja9210 <pp506345@outlook.com>
2025-03-18 23:16:04 +0530

    Update dfs.py

--- practical 2/dfs.py
+++ practical 2/dfs.py
@@ -4,7 +4,7 @@ class TreeNode:
 		self.left = None
 		self.right = None
 
-# Create nodes
+
 A = TreeNode('A')
 B = TreeNode('B')
 C = TreeNode('C')
@@ -12,20 +12,19 @@ D = TreeNode('D')
 E = TreeNode('E')
 F = TreeNode('F')
 
-# Connect nodes
 A.left = B
 A.right = C
 B.left = D
 B.right = E
 C.right = F
 
-# DFS Traversal Function
+
 def dfs(node):
 	if node is not None:
-		print(node.value)  # Process the node (e.g., print it)
-		dfs(node.left)     # Traverse left subtree
-		dfs(node.right)    # Traverse right subtree
+		print(node.value) 
+		dfs(node.left)    
+		dfs(node.right)   
 
-# Perform DFS traversal
+
 print("DFS Traversal of the tree:")
 dfs(A)
